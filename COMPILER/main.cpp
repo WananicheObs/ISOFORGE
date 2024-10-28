a29284380b5cc3cfc2bfd936b51bc7ea70b109aa
WananicheObs <155215757+WananicheObs@users.noreply.github.com>
2024-10-27 22:42:23 -0400

    #

---
+++ COMPILER/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // Include the iostream library for input and output
+#include <ISOFORGEconfig.h>
+
+int main() {
+    // Print "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << p_NAME << "  --VERSION: " << p_VERSION_MAJOR << "." << p_VERSION_MINOR << std::endl;
+    // Return 0 to indicate successful execution
+    return 0;
+}
